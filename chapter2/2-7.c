602ae9727c9c8c8adef08bc880596320ff262b92
2085099 <2085099m@student.gla.ac.uk>
2016-09-03 21:05:25 +0100

    2-7

--- chapter2/2-7.c
+++ chapter2/2-7.c
@@ -1,7 +1,7 @@
-/* Exercise 2-6
- * Write a function setbits(x,p,n,y) that returns x with the n
- * bits that begin at position p set to the rightmost n bits of
- * y, leaving the other bits unchanged
+/* Exercise 2-7
+ * Write a function invert(x,p,n) that returns x with the n bits
+ * that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving
+ * the others unchanged.
  */
 
 #include <stdio.h>
