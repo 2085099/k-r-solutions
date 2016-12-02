019fb3cbf346c5a03671aa5328a37541ee14f654
maximilian <2085099m@student.gla.ac.uk>
2016-12-02 11:06:03 +0000

    8-1

---
+++ chapter8/8-1.c
@@ -0,0 +1,38 @@
+/* Exercise 8-1
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> 
+#include <sys/file.h>
+
+void error(char *);
+
+/* cat: concatenate files */
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	int n;
+	void filecopy(FILE *, FILE *);
+	char *prog = argv[0];
+	char buf[BUFSIZ];
+	
+	if (argc == 1)
+		filecopy(stdin, stdout);
+	else
+		while (--argc > 0)
+			if ((fd = open(*++argv, O_RDONLY)) == -1) {
+				error("can't open file");
+			} else {
+				while ((n = read(fd, buf, BUFSIZ)) > 0){
+					if (write(1, buf, n) != n){
+						error("write error on file");
+					}				
+				}
+			}
+
+		exit(0); // Like close(), exit closes all open files
+}
