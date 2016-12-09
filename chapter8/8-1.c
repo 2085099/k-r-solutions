32a35c4fc1a19fd6026413146c63dc7fa370a066
maximilian <2085099m@student.gla.ac.uk>
2016-12-09 12:29:31 +0000

    Chapter 8

--- chapter8/8-1.c
+++ chapter8/8-1.c
@@ -1,5 +1,8 @@
 /* Exercise 8-1
- * 
+ * Rewrite the program cat from Chapter 7 using read, write,
+ * open and close instead of their standard library equivalents.
+ * Perform experiments to determine the relative speed of the
+ * two versions.
  */
 
 #include <stdio.h>
